753f0350239a07ea04684bca35f6b46b6577146d
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-03 17:04:27 +0900

    fixed clock

--- js/clock.js
+++ js/clock.js
@@ -7,10 +7,16 @@ function getClock(){
     //const minutes = date.getMinutes();
     //const seconds = date.getSeconds();
     //clock.innerText = `${hours<10 ? `0${hours}`:hours}:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`:seconds}`;
-    const hours =String( date.getHours()).padStart(2,"0");
+    const hours =String(date.getHours()).padStart(2,"0");
+    const intHours = parseInt(hours);
+    
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    if(intHours >= 12){
+        clock.innerText = `${String(intHours-12).padStart(2,"0")}:${minutes}:${seconds} PM`;
+    }else{
+        clock.innerText = `${hours}:${minutes}:${seconds} AM`;
+    }
 }
 getClock();
 setInterval(getClock, 1000);
