78790a11764f3e13ac2ebdde357bf3513f9ecff2
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-10-03 18:49:07 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -7,9 +7,7 @@ function getClock(){
     //const minutes = date.getMinutes();
     //const seconds = date.getSeconds();
     //clock.innerText = `${hours<10 ? `0${hours}`:hours}:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`:seconds}`;
-    const hours =String(date.getHours()).padStart(2,"0");
-    const intHours = parseInt(hours);
-    
+    const hours =String(date.getHours()).padStart(2,"0"); 
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
     const intHours = parseInt(hours);
